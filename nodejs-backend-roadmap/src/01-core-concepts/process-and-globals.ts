67dd7f6a3237e41353370c65abc6916239ed7bc3
sagarpb1201 <sagarpb1201@gmail.com>
2025-09-08 22:14:01 +0530

    Errors and process

--- nodejs-backend-roadmap/src/01-core-concepts/process-and-globals.ts
+++ nodejs-backend-roadmap/src/01-core-concepts/process-and-globals.ts
@@ -1,4 +1,30 @@
+import path from 'node:path'
 console.log(process.version)
 console.log(process.argv)
 console.log(process.env.API_KEY)
 console.log(process.env)
+
+process.on('exit',(code)=>{
+    console.log(code);
+})
+
+process.on('uncaughtException',(err,origin)=>{
+    console.error(err)
+    console.error("originnnnnn",origin)
+    process.exit(1);
+})
+
+console.log('Current Working Directory:', process.cwd());
+console.log('Directory Name of Current Module:', __dirname);
+
+console.log(
+  'Path to config.json using process.cwd():',
+  path.join(process.cwd(), 'config.json')
+);
+
+console.log(
+  'Path to config.json using __dirname:',
+  path.join(__dirname, 'config.json')
+);
+
+throw new Error('This is a deliberate crash')
